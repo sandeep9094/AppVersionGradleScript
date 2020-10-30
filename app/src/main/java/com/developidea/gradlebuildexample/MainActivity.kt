6c10396535cf4e67bf622a21d1d363d88769c3c4
SANDEEP KUMAR <sandeepkumar9094@gmail.com>
2020-10-31 00:26:54 +0530

    apk release file name updated with versionName

--- app/src/main/java/com/developidea/gradlebuildexample/MainActivity.kt
+++ app/src/main/java/com/developidea/gradlebuildexample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.developidea.gradlebuildexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appBuildTextView : TextView = findViewById(R.id.textview)
+        appBuildTextView.text = "App Build Version : ${BuildConfig.VERSION_NAME}"
     }
 }
